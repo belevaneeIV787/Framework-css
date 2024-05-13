fed6aa71c827953db1384fc105463b18efd14883
Isabele Vanessa <138811584+belevaneeIV787@users.noreply.github.com>
2024-05-13 17:42:06 -0300

    Add files via upload

---
+++ Framework-css/src/Components/Style/C1/Textos.js
@@ -0,0 +1,29 @@
+import  styled  from "styled-components";
+
+export const Caixa = styled.div`
+   display: flex;
+   flex-direction: column;
+   padding: 20px;
+   justify-content: center;
+   background-color: azure;
+   width: 540px;
+  height: 220px;
+ border-top-left-radius: 10px;
+ border-top-right-radius: 10px;
+`
+
+export const Texto1 = styled.h1`
+
+    color:  hsl(179, 62%, 43%);
+`
+export const Texto2 = styled.h2`
+    color:  hsl(71, 73%, 54%);
+`
+
+export const Para = styled.p`
+padding: 0;
+margin: 0;
+    color: hsl(218, 22%, 67%);
+`
+
+
---
+++ Framework-css/src/Components/Style/C2/Textos2.js
@@ -0,0 +1,50 @@
+import  styled  from "styled-components";
+
+export const Caixa = styled.div`
+   display: flex;
+   flex-direction: column;
+   padding: 20px;
+   border-bottom-left-radius: 10px;
+   justify-content: center;
+   background-color: hsl(179, 62%, 43%) ;
+   width: 250px;
+  height: 250px;
+`
+
+export const Texto1 = styled.h1`
+    color:  #fff;
+    font-size:20px ;
+    padding-bottom: 10px;
+  margin: 0;
+
+`
+export const Span = styled.span`
+  display: flex; 
+`
+
+export const Valor = styled.h2`
+    color: #fff;
+   
+`
+export const ValorP = styled.p`
+    color:  #fff ;
+    margin-left: 14px;
+    margin-top: 25px;
+    color:  rgb(154, 227, 227);
+    `
+export const Para = styled.p`
+    color: #fff;
+    
+`
+
+ export const Button = styled.button`
+    color: aliceblue;
+    padding: 13px;
+    border: none;
+   background-color:hsl(71, 73%, 54%) ;
+   border-radius: 5px;
+   align-items: center;
+
+ `
+
+
